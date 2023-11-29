45e9fcb46e605d1dd64398c882adf45deb91137a
vdamaraju12 <vdamaraju@wisc.edu>
2023-11-29 17:34:46 -0600

    more updates

--- app/src/main/java/com/cs407/here/MainActivity.java
+++ app/src/main/java/com/cs407/here/MainActivity.java
@@ -1,12 +1,10 @@
 package com.cs407.here;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 
