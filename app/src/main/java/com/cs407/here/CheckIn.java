03ddd19f5d67baccc5d0d964c8429240c0047762
vignagajarla <gajarla@wisc.edu>
2023-11-29 10:29:45 -0600

    Added pages

---
+++ app/src/main/java/com/cs407/here/CheckIn.java
@@ -0,0 +1,14 @@
+package com.cs407.here;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CheckIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_in);
+    }
+}
