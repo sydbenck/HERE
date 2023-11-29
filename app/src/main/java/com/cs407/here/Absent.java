816fd60a1093b3073aab5830de2ab3d6a0524a7a
vignagajarla <gajarla@wisc.edu>
2023-11-29 16:20:42 -0600

    buttons work

--- app/src/main/java/com/cs407/here/Absent.java
+++ app/src/main/java/com/cs407/here/Absent.java
@@ -2,7 +2,9 @@ package com.cs407.here;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Absent extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class Absent extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_absent);
+    }
+    public void goToClassList(View view){
+        Intent intent = new Intent(this, ClassList.class);
+        startActivity(intent);
     }
 }
