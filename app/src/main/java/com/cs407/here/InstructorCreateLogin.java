e800a1c8eca634565909689c4d9182a375183dbb
vdamaraju12 <vdamaraju@wisc.edu>
2023-12-10 15:25:48 -0600

    even even more updates

---
+++ app/src/main/java/com/cs407/here/InstructorCreateLogin.java
@@ -0,0 +1,25 @@
+package com.cs407.here;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class InstructorCreateLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instructor_create_login);
+    }
+    public void goToStudentList(View view) {
+        Intent intent = new Intent(this, StudentList.class);
+        startActivity(intent);
+    }
+
+    public void goToInstructorCreate(View view) {
+        Intent intent = new Intent(this, InstructorCreatePage.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/cs407/here/InstructorCreatePage.java
@@ -0,0 +1,31 @@
+package com.cs407.here;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class InstructorCreatePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instructor_create_page);
+    }
+
+    public void goToInstructorLoginPage(View view) {
+        EditText instructor_name = (EditText) findViewById(R.id.instructorName);
+        EditText instructor_class = (EditText) findViewById(R.id.instructorClass);
+        Context context = getApplicationContext();
+        SQLiteDatabase sqLiteDatabase = context.openOrCreateDatabase("users", Context.MODE_PRIVATE, null);
+        DBHelper dbHelper = new DBHelper(sqLiteDatabase);
+        dbHelper.createAccount(""+(instructor_name.getText()), "instructor", "0", ""+(instructor_class.getText()), "", "");
+        Intent intent = new Intent(this, InstructorCreateLogin.class);
+        startActivity(intent);
+    }
+
+}
